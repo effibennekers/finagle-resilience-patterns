3068df12f919f26f7a1e7474717babfa9a8c3242
Effi Bennekers <efraim.bennekers@ing.com>
2018-10-22 19:02:14 -0700

    Renames, delombok, slf4j

--- client/src/main/java/org/effiandeggie/jfall/instances/Instance.java
+++ client/src/main/java/org/effiandeggie/jfall/instances/Instance.java
@@ -1,18 +1,30 @@
 package org.effiandeggie.jfall.instances;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class Instance {
 
     private final String name;
     private final String host;
     private final int port;
 
+    public Instance(String name, String host, int port) {
+        this.name = name;
+        this.host = host;
+        this.port = port;
+    }
+
     public String getKey() {
         return host + ":" + port;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
 }
